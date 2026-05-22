b7ffb622d3f5a6f1efd355413be3b90a71e15000
mahataanwesha <mahataanwesha35@gmail.com>
2026-05-22 23:05:50 +0530

    fix(types): resolve typescript build errors

--- src/utils/generateToken.ts
+++ src/utils/generateToken.ts
@@ -2,7 +2,7 @@ import jwt from 'jsonwebtoken';
 
 const generateToken = (id: string) => {
   return jwt.sign({ id }, process.env.JWT_SECRET as string, {
-    expiresIn: process.env.JWT_EXPIRE || '30d',
+    expiresIn: (process.env.JWT_EXPIRE || '30d') as any,
   });
 };
 
